12d959bef654dc8070ff3c6c5960a6d2ec899746
Pranish Chaulagain <153913535+Pranish03@users.noreply.github.com>
2026-09-03 22:19:59 +0545

    feat: integrate CORS and update dependencies for improved security and functionality

--- index.ts
+++ index.ts
@@ -1,12 +1,25 @@
 import "dotenv/config";
 import express, { type Request, type Response } from "express";
+import cors from "cors";
 import { createServer } from "node:http";
 import { Server } from "socket.io";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth.js";
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
+const PORT = process.env.PORT || 3000;
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL!,
+    credentials: true,
+  }),
+);
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
@@ -18,8 +31,6 @@ app.get("/", (req: Request, res: Response) => {
 io.on("connection", () => {
   console.log("User connected");
 });
-
-const PORT = 3000;
 
 server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
