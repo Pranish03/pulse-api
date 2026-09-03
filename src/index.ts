2d63dbb218b00a773cce481453f211619e9c4908
Pranish Chaulagain <153913535+Pranish03@users.noreply.github.com>
2026-09-03 21:49:33 +0545

    Initialize backend with Express, TypeScript, and basic configuration
    - Created package.json with necessary scripts and dependencies
    - Added TypeScript configuration in tsconfig.json
    - Implemented a basic Express server in src/index.ts that responds with "Hello World!"

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello World!",
+  });
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
