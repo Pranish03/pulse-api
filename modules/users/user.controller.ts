2c509e54281dac5532eb80b3ab8791c89f3fa8f9
Pranish Chaulagain <153913535+Pranish03@users.noreply.github.com>
2026-09-05 18:02:15 +0545

    feat: implement profile update functionality with validation using Zod

--- modules/users/user.controller.ts
+++ modules/users/user.controller.ts
@@ -1,9 +1,43 @@
 import type { Request, Response } from "express";
+import { profileUpdateSchema, type UpdateProfile } from "./user.schema.js";
+import z from "zod";
+import { auth } from "../../lib/auth.js";
+import { fromNodeHeaders } from "better-auth/node";
 
 export function getProfile(req: Request, res: Response) {
   return res.status(200).json({ data: req.user });
 }
 
-export function updateProfile() {}
+export async function updateProfile(
+  req: Request<{}, {}, UpdateProfile>,
+  res: Response,
+) {
+  try {
+    const data = req.body;
+    const parsedData = z.safeParse(profileUpdateSchema, data);
+
+    if (!parsedData.success)
+      return res.status(400).json({
+        message: "Validation error",
+        error: z.prettifyError(parsedData.error),
+      });
+
+    const { name, image } = parsedData.data;
+
+    const updateBody: Record<string, unknown> = {};
+    if (name !== undefined) updateBody.name = name;
+    if (image !== undefined) updateBody.image = image;
+
+    const updatedUser = await auth.api.updateUser({
+      body: updateBody,
+      headers: fromNodeHeaders(req.headers),
+    });
+
+    return res.status(200).json({ data: updatedUser });
+  } catch (error) {
+    console.error("Update profile failed:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+}
 
 export function searchUser() {}
---
+++ modules/users/user.schema.ts
@@ -0,0 +1,13 @@
+import z from "zod";
+
+export const profileUpdateSchema = z
+  .object({
+    name: z
+      .string()
+      .min(3, "Name must be at least 3 characters")
+      .max(30, "Name must be at most 30 characters"),
+    image: z.url("Invalid image URL").nullable(),
+  })
+  .partial();
+
+export type UpdateProfile = z.infer<typeof profileUpdateSchema>;
