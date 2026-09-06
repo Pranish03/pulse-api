05d4fab2195d0512609f2dc5222e394546f30bdf
Pranish Chaulagain <153913535+Pranish03@users.noreply.github.com>
2026-09-06 14:34:34 +0545

    fix: improve error handling formatting and structure in error middleware

--- middlewares/error.middleware.ts
+++ middlewares/error.middleware.ts
@@ -1,19 +1,27 @@
 import type { NextFunction, Request, Response } from "express";
 import { MulterError } from "multer";
 
+import { AppError } from "../lib/errors.js";
+
 export function errorHandler(
   err: unknown,
   req: Request,
   res: Response,
   next: NextFunction,
 ) {
-  console.error("Unhandled error: ", err);
+  console.error("Unhandled error:", err);
 
-  if (err instanceof MulterError)
-    return res.status(400).json({ message: err.message });
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({ message: err.message });
+  }
 
-  if (err instanceof Error)
+  if (err instanceof MulterError) {
     return res.status(400).json({ message: err.message });
+  }
+
+  if (err instanceof Error) {
+    return res.status(400).json({ message: err.message });
+  }
 
   return res.status(500).json({ message: "Internal server error" });
 }
