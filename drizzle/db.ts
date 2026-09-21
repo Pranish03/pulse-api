2da924df441a025778590eafa93817d14c87a865
Pranish Chaulagain <153913535+Pranish03@users.noreply.github.com>
2026-09-21 12:32:23 +0545

    feat: switch database driver to node-postgres and update dependencies

--- drizzle/db.ts
+++ drizzle/db.ts
@@ -1,5 +1,8 @@
-import { drizzle } from "drizzle-orm/neon-http";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 import * as schema from "./schema.js";
 import { DATABASE_URL } from "../config/constants.js";
 
-export const db = drizzle(DATABASE_URL, { schema });
+const pool = new Pool({ connectionString: DATABASE_URL });
+
+export const db = drizzle(pool, { schema });
